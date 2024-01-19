7c24701258583fbaabab15cf243d40081c6afa3e
Trisha-P <trisha.panchal@live.ca>
2024-01-19 11:06:15 -0500

    build: walking skeleton

--- src/main/java/ca/mcscert/se2aa4/tools/mazegen/Configuration.java
+++ src/main/java/ca/mcscert/se2aa4/tools/mazegen/Configuration.java
@@ -1,4 +1,11 @@
 package ca.mcscert.se2aa4.tools.mazegen;
 
-public record Configuration() {
+import java.io.BufferedWriter;
+
+public record Configuration(int width, int height, BufferedWriter out, long seed, boolean humanReadable) {
+    public Configuration {
+    }
+    public static Configuration load(String[] args) {
+        return null;
+    }
 }
