c5f00358e6a336e17d820685b4b68034c29593c4
Trisha-P <trisha.panchal@live.ca>
2024-01-19 10:41:49 -0500

    build: walking skeleton

---
+++ src/main/java/ca/mcscert/se2aa4/tools/mazegen/Main.java
@@ -0,0 +1,13 @@
+package ca.mcscert.se2aa4.tools.mazegen;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("Here goes the business code");
+    }
+}
